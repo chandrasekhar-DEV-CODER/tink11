54872b5fa47c277291f4faaf4c90efb4cc8a67fd
chandrasekhar-DEV-CODER <chandrasekharsathi.dev@gmail.com>
2025-12-03 19:26:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,26 @@ import path from 'path';
 
 import { miaodaDevPlugin } from "miaoda-sc-plugin";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
+  plugins: [
+    react(),
+    svgr({
       svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+        icon: true,
+        exportType: 'named',
+        namedExport: 'ReactComponent',
+      },
+    }),
+    miaodaDevPlugin(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['bcryptjs'],   // 👈 tell Rollup not to bundle bcryptjs
     },
   },
 });
